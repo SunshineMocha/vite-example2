30d3387446aefb89bc0d5f615060bc0781740e90
SunshineMocha <127298037+SunshineMocha@users.noreply.github.com>
2023-05-03 11:24:56 +0200

    finished

--- main.js
+++ main.js
@@ -1,11 +1,60 @@
 import './style.css'
+import {CatService} from "./services/cat-service"
+import confetti from 'canvas-confetti';
 
-console.log('hello vite')
+// CatService.getBreeds().then(data => {
+//   console.log(data)
+//   displayBreeds(data)
+//   fireConfetti()
+// });
 
-function meow(){
-  console.log("meow");
+// CatService.getFacts().then(data => {
+//   console.log(data)
+//   displayFacts(data)
+//   fireConfetti()
+// });
+
+function meowBreeds() {
+  //console.log("meow");
+  CatService.getBreeds().then(data => {
+    //console.log(data)
+    displayBreeds(data)
+    fireConfetti()
+  });
 }
 
-const meowBtn = document.getElementById('cat-btn');
+function meowFacts() {
+  //console.log("meow");
+  CatService.getFacts().then(data => {
+    //console.log(data)
+    displayFacts(data)
+    fireConfetti()
+  });
+}
 
-meowBtn.addEventListener('click', meow)
+const meowBreedBtn = document.getElementById('catbreeds-btn');
+const meowFactsBtn = document.getElementById('catfacts-btn');
+
+meowBreedBtn.addEventListener('click', meowBreeds)
+meowFactsBtn.addEventListener('click', meowFacts)
+
+function displayFacts(data){
+  const container = document.getElementById('cat-container');
+  for(const factObj of data) {
+      container.innerHTML += `<li>${factObj.fact}</li>`
+  }
+}
+function displayBreeds(data){
+  const container = document.getElementById('cat-container');
+  for(const breedObj of data) {
+      container.innerHTML += `<li>${breedObj.breed}</li>`
+  }
+}
+
+function fireConfetti(){
+  confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: { y: 0.6 }
+  });
+}
--- services/cat-service.js
+++ services/cat-service.js
@@ -1,20 +1,40 @@
-class PokeService{
+export class CatService{
     
     static BASE_URL = 'https://catfact.ninja'
 
-    static getPage(index){
-        const url = this.BASE_URL + '/breed';
-        return fetch(url).then(resp => resp.json()).then(catPage => this.getDetails(catPage.results))
+    static getBreeds(){
+        const url = this.BASE_URL + '/breeds';
+        
+        return fetch(url)
+        .then(resp => resp.json())
+        .then(data => this.elaborateData(data));
     }
+    static getFacts(){
+        const url = this.BASE_URL + '/facts';
+        
+        return fetch(url)
+        .then(resp => resp.json())
+        .then(data => this.elaborateData(data));
+    }
+    static elaborateData(obj){
+        // console.log('obj', obj);
+        const data = obj.data;
+        // console.log('facts', facts);
+        //const first6 = facts.filter((f, i) => i < 6);
+        const first6 = this.getFirst6(data);
+        return first6;
+    }
+    static getFirst6(array){
+        const tempArray = [];
 
-    static getDetails(catsBreed){
-        const requests = [];
-        for (const cats of catsBreed) {
-            const name = cats.name;
-            const url = this.BASE_URL + 'breeds/' + name;
-            const request = fetch(url).then(resp => resp.json());
-            requests.push(request);
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+            if (i < 6) {
+                tempArray.push(element);
+            } else{
+                break;
+            }    
         }
-        return Promise.all(requests) // lancia tutte le chiamate che abbiamo fatto nell'array
+        return tempArray;
     }
 }
